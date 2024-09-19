1d9ea048ce4211c6e6bbfece6bc1fd8e12a919e3
Muhammad-Qasim-Siddiqui <mqasim458.mq@gmail.com>
2024-09-20 03:22:28 +1000

    Added deployment.yaml and a few more updates

--- project_part3/book_catalog/app/database.py
+++ project_part3/book_catalog/app/database.py
@@ -3,9 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+# Updated to include sslmode=require for secure SSL connection
+SQLALCHEMY_DATABASE_URL = "postgresql://sit722_part3_db_user:gS4ZrYN42FYsXTyfTnaHpELURuhXNcDb@dpg-crm4hgij1k6c73fjgb1g-a.oregon-postgres.render.com/sit722_part3_db?sslmode=require"
 
+# Create the SQLAlchemy engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Set up the session maker
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base model to inherit from
 Base = declarative_base()
--- project_part3/inventory_management/app/config.py
+++ project_part3/inventory_management/app/config.py
@@ -1,3 +1,4 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+# Ensure DATABASE_URL can be fetched from environment variables
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://sit722_part3_db_user:gS4ZrYN42FYsXTyfTnaHpELURuhXNcDb@dpg-crm4hgij1k6c73fjgb1g-a.oregon-postgres.render.com/sit722_part3_db?sslmode=require')
